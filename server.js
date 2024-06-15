cd29032d0bbc91a4694cbf4aa69c136cf7afe305
BasiliscX <juniornavarro1988@gmail.com>
2024-06-15 20:14:05 -0300

    Start of 'web-interface' branch.

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 import express from 'express';
-import Routes from './src/routes.js';
-import bodyParser from 'body-parser'
+import { fetchScores } from './src/features/scores/scores.service.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+// Middleware para analizar el cuerpo de las solicitudes POST
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/', Routes);
+// Servir archivos estáticos desde la carpeta public
+app.use(express.static('public'));
+
+// Ruta para la API
+app.post('/api/scores', async (req, res) => {
+    const { facultad, legajo, password } = req.body;
+
+    try {
+        const scores = await fetchScores({ facultad, legajo, password });
+        res.json(scores);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+// Ruta para la página principal
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
 
 app.listen(port, () => {
-  console.log(`Servidor Express escuchando en el puerto ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
