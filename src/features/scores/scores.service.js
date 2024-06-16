3c4fe7f9c759cfcd49d4bd7666f9c20e6a9b357b
BasiliscX <juniornavarro1988@gmail.com>
2024-06-15 21:33:03 -0300

    executablePath route.

--- src/features/scores/scores.service.js
+++ src/features/scores/scores.service.js
@@ -1,10 +1,11 @@
 import puppeteer from 'puppeteer';
 
 export async function fetchScores(data){
-    
+
     async function startBrowser() {
         const browser = await puppeteer.launch({
             headless: true,
+            executablePath: '/usr/bin/google-chrome-stable',
             args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
         return browser;
